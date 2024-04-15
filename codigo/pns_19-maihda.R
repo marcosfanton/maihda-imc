3417a6ab9f9097338707c7821d5cd9284b8139a4
marcosfanton <marcos.fanton@ufsm.br>
2024-04-15 15:37:36 -0300

    Análise com base em Evans 2024. Variáveis: gênero-raça-renda. Verificar idade.

--- codigo/pns_19-maihda.R
+++ codigo/pns_19-maihda.R
@@ -6,7 +6,6 @@ library(skimr)
 # Construção do banco de dados####
 # Seleção de variáveis
 vars <- c(
-  "V00291", # peso do morador selecionado
   "P005", # gravidez (2 = NÃO)
   "C006", # sexo/gênero
   "C008", # idade 
@@ -38,7 +37,6 @@ pns19 <- raw_19 |>
   dplyr::filter(P00102 == "Sim" & P00402 == "Sim")  |>  # sabe o peso & a altura
   dplyr::filter(P005 != "Sim"| is.na(P005))  |>  # exclusão de mulheres grávidas
   dplyr::mutate(
-    peso_original = V00291,
     age = factor(
       cut(C008, c(breaks = quantile(C008, 
                                     probs = seq(0, 1, 1/4), 
@@ -60,28 +58,22 @@ pns19 <- raw_19 |>
       cut(VDF003, c(breaks = quantile(VDF003, 
                                       probs = seq(0, 1, 1/4), 
                                       na.rm = TRUE)), 
-          labels = c("ilow", "imiddle_low", "imiddle_high", "ihigh"), # variável renda por tercil
+          labels = c("low", "middle_low", "middle_high", "high"), # variável renda por tercil
           right = FALSE, 
           include.lowest = TRUE)),
-    education = factor(case_when(
-      VDD004A %in% c("Sem instrução", "Fundamental incompleto ou equivalente") ~ "elow",
-      VDD004A %in% c("Fundamental completo ou equivalente", 
-                     "Médio incompleto ou equivalente") ~ "emiddle_low",
-      VDD004A == "Médio completo ou equivalente" ~ "emiddle_high",
-      VDD004A %in% c("Superior incompleto ou equivalente", "Superior completo") ~ "ehigh"),
-      levels = c("elow", "emiddle_low", "emiddle_high", "ehigh")), # variável escolaridade por quartil
     bmi = P00103/((P00403/100)^2), # variável IMC
     obesity = factor(case_when(  
       bmi >= 30 ~ 1,
       TRUE ~ 0), 
       levels = c(0, 1)))  |>  # variável estado nutricional (obesidade = sim/não)
-  select(age, gender, race, education, income, bmi, obesity, peso_original) # seleção das variáveis
+  select(age, gender, race, education, bmi, obesity) # seleção das variáveis
 
 pns19 <- drop_na(pns19)
 
 # Criação de estratos sociais
 pns19 <- pns19  |> 
-  dplyr::mutate(strata = paste(gender, race, income, education, sep = ""))
+  dplyr::mutate(strata = paste(gender, race, income, education, sep = "")) |> 
+  mutate(strata = factor(strata))
 
 # Salvar banco
 write.csv(pns19, 
@@ -90,4 +82,26 @@ write.csv(pns19,
 
 # Carregar banco
 raw_19 <- read.csv("dados/raw_19.csv")
+
+# Análise - Tutorial: ####
+# IMC ####
+# Modelo 1A | Regressão linear com dois níveis com covariáveis#### 
+model1A <- lmer(bmi ~ (1|stratum), 
+                data=pns19)
+
+# Modelo 1B | Regressão linear com dois níveis sem covariáveis#### 
+model1B <- lmer(bmi ~ gender + race + income + (1|stratum), 
+                data=pns19)
+
+# OBESIDADE ####
+# Modelo 2A | Regressão logística com dois níveis com covariáveis#### 
+model2A <- glmer(obesity ~ (1|stratum), 
+                 data=pns19, 
+                 family=binomial)
+
+# Modelo 2B | Regressão logística com dois níveis com covariáveis#### 
+model2B <- glmer(obesity ~ gender + race + income +
+                   (1|stratum), 
+                 data=pns19, 
+                 family=binomial)
 
